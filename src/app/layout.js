8957a9e92811ea3ae9517cc433feb42031ce85ee
Android Studio <devandroid@ort>
2024-09-04 08:42:45 -0300

    base

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Directorio de personas",
+  description: "Creado por Marco A.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
